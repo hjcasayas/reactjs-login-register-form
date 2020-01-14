97e517a259db32cadb987ae9b52916ae9adb95de
hjcasayas <henlyjade.casayas@gmail.com>
2020-01-14 18:16:56 +0800

    Updates

---
+++ precache-manifest.cdba9a5f6cf363ba031a0bedd2ff8f44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c94b3d245137047c43e40cc1ce73e5b",
+    "url": "/reactjs-login-register-form/index.html"
+  },
+  {
+    "revision": "56f50f88ace3d27361e5",
+    "url": "/reactjs-login-register-form/static/css/2.624e4bf4.chunk.css"
+  },
+  {
+    "revision": "2b92fe2989146ad70fcb",
+    "url": "/reactjs-login-register-form/static/css/main.fd04296b.chunk.css"
+  },
+  {
+    "revision": "56f50f88ace3d27361e5",
+    "url": "/reactjs-login-register-form/static/js/2.99fc6b9f.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/reactjs-login-register-form/static/js/2.99fc6b9f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2b92fe2989146ad70fcb",
+    "url": "/reactjs-login-register-form/static/js/main.3d2b7eea.chunk.js"
+  },
+  {
+    "revision": "96eb7736445281e91024",
+    "url": "/reactjs-login-register-form/static/js/runtime-main.d1569eef.js"
+  }
+]);
